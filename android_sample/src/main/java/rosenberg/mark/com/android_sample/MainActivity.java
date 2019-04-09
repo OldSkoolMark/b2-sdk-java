573460b5361d57ee4a486cd384a4c9cc57a1d151
mark rosenberg <mark@backblaze.com>
2019-04-09 16:05:38 -0700

    Add upload to cloud FAB

--- android_sample/src/main/java/rosenberg/mark/com/android_sample/MainActivity.java
+++ android_sample/src/main/java/rosenberg/mark/com/android_sample/MainActivity.java
@@ -1,6 +1,10 @@
 package rosenberg.mark.com.android_sample;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentTransaction;
@@ -11,6 +15,14 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.add(R.id.listcontainer, BucketListFragment.newInstance());
         ft.commit();
